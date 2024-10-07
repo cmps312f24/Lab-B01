a8e56a2884d2154eabebd5de493cfedb4e0d26ed
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-10-07 10:22:57 +0300

    updated route

---
+++ Lab Practice/Lab 7 Navigations and State Management/Tutorial/qbanking_app/lib/routes/app_router.dart
@@ -0,0 +1,46 @@
+import 'package:go_router/go_router.dart';
+import 'package:qbanking_app/screens/account_screen.dart';
+import 'package:qbanking_app/screens/deposit_screen.dart';
+import 'package:qbanking_app/screens/home_screen.dart';
+import 'package:qbanking_app/screens/transfer_screen.dart';
+
+class AppRouter {
+  static const home = (name: 'home:id', path: '/');
+  static const account = (name: 'account', path: 'account');
+  static const deposit = (name: 'deposit', path: 'deposit:accountNo');
+  static const transaction = (name: 'transaction', path: 'transaction');
+  static const transfer = (name: 'transfer', path: 'transfer');
+
+  // router
+
+  static final GoRouter router = GoRouter(
+    initialLocation: home.path,
+    routes: [
+      GoRoute(
+        name: home.name,
+        path: home.path,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            name: account.name,
+            path: account.path,
+            builder: (context, state) => const AccountScreen(),
+          ),
+          GoRoute(
+            name: deposit.name,
+            path: deposit.path,
+            builder: (context, state) {
+              final accountNo = state.pathParameters['accountNo'] ?? '';
+              return DepositScreen(accountNo: accountNo);
+            },
+          ),
+          GoRoute(
+            name: transfer.name,
+            path: transfer.path,
+            builder: (context, state) => const TransferScreen(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
