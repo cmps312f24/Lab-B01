35bf38babc2ec6a7488efa74a03719a759750b17
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-11-18 10:28:08 +0300

    database view

--- Lab Practice/Lab 10 - DataLayer/Tutorial/todo_list_base/lib/model/project_todo_status_counts.dart
+++ Lab Practice/Lab 10 - DataLayer/Tutorial/todo_list_base/lib/model/project_todo_status_counts.dart
@@ -2,12 +2,18 @@ import 'package:floor/floor.dart';
 //use sum if
 
 // Todo : add the @Databaseview annotation
-@DatabaseView(
-  '''
-    
-  
-  '''
-)
+@DatabaseView('''
+  SELECT
+    p.id as id,
+    IFNULL(SUM(CASE WHEN t.status = 'Pending' THEN 1 ELSE 0 END), 0) as pendingCount,
+    IFNULL(SUM(CASE WHEN t.status = 'Completed' THEN 1 ELSE 0 END), 0) as completedCount
+  FROM projects p
+  LEFT JOIN todos t ON p.id = t.pid
+  GROUP BY p.id
+''', viewName: 'ProjectTodoStatusCounts')
+@DatabaseView('''
+
+''')
 class ProjectTodoStatusCounts {
   @primaryKey
   final int id;
