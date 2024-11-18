da6ac86efcf6b7a0b19c99f6d527b3e886c64459
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-11-18 10:52:41 +0300

    updated

--- Lab Practice/Lab 10 - DataLayer/Tutorial/todo_list_base/lib/database/project_dao.dart
+++ Lab Practice/Lab 10 - DataLayer/Tutorial/todo_list_base/lib/database/project_dao.dart
@@ -3,3 +3,24 @@ import 'package:todo_list_base/model/project.dart';
 import 'package:todo_list_base/model/project_todo_status_counts.dart';
 
 // Todo : add the ProjectDao class
+abstract class ProjectDao {
+  @Query("SELECT * FROM projects")
+  Stream<List<Project>> observeProjects();
+
+  @Query("SELECT * FROM projects WHERE id = :id")
+  Future<Project?> findProjectById(int id);
+
+  @Insert(
+    onConflict: OnConflictStrategy.replace,
+  )
+  Future<void> insertProject(Project project);
+
+  @update
+  Future<void> updateProject(Project project);
+
+  @delete
+  Future<void> deleteProject(Project project);
+
+  @Query('SELECT * FROM projects WHERE id = :pid')
+  Stream<ProjectTodoStatusCounts> observeProjectTodoStatusCounts(int pid);
+}
